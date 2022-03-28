b378fb627f05f340c3b83ff5c94a2aeaac189177
norasotanext <minhbn@sotanext.com>
2022-03-28 17:11:51 +0700

    add email constants events

---
+++ email.constants.ts
@@ -0,0 +1,6 @@
+export const EMAIL_CONST_EVENTS = {
+  EMAIL_REPORT: "email.report",
+  EMAIL_USER_VERIFICATION: "email.user_verification",
+  EMAIL_USER_FORGOT_PASSWORD: "email.user_forgot_password",
+  EMAIL_PARTNERSHIP_FORM: "email.partnership_form",
+};
