af09561bb1ae6302579c25299892330410687895
norasotanext <minhbn@sotanext.com>
2022-03-29 14:27:11 +0700

    update

--- charge.constants.ts
+++ charge.constants.ts
@@ -1,7 +1,8 @@
 export const CHARGE_CONST_EVENTS = {
   CHARGE_CREATED: "charge.created",
-  // CHARGE_UPDATED: 'charge.updated',
-  CHARGE_CANCEL: "charge.cancel",
-  CHARGE_RESOLVE: "charge.resolve",
-  CHARGE_REFUND: "charge.refund",
+  CHARGE_CANCELLED: "charge.cancelled",
+  CHARGE_RESOLVED: "charge.resolved",
+  CHARGE_UNRESOLVED: "charge.unresolved",
+  CHARGE_REFUNDED: "charge.refunded",
+  CHARGE_COMPLETED: "charge.completed",
 };
