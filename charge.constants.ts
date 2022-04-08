992b48e236da4ffd2182854ac8817a53698ebf4e
norasotanext <minhbn@sotanext.com>
2022-04-08 10:47:47 +0700

    chore: rename variable

--- charge.constants.ts
+++ charge.constants.ts
@@ -1,8 +1,8 @@
 export const CHARGE_CONST_EVENTS = {
-  CHARGE_CREATED: "charge.created",
-  CHARGE_CANCELLED: "charge.cancelled",
-  CHARGE_RESOLVED: "charge.resolved",
-  CHARGE_UNRESOLVED: "charge.unresolved",
-  CHARGE_REFUNDED: "charge.refunded",
-  CHARGE_COMPLETED: "charge.completed",
+  CHARGE_CREATED: 'charge.created',
+  CHARGE_CANCELED: 'charge.canceled',
+  CHARGE_RESOLVED: 'charge.resolved',
+  CHARGE_UNRESOLVED: 'charge.unresolved',
+  CHARGE_REFUNDED: 'charge.refunded',
+  CHARGE_COMPLETED: 'charge.completed',
 };
