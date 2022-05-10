be4ab0eabb8fe44eea92a97e3ce46247801c8acd
norasotanext <minhbn@sotanext.com>
2022-05-10 09:53:52 +0700

    feat: add cookie header

--- sso/cookie.ts
+++ sso/cookie.ts
@@ -1,3 +1,4 @@
-export const FP_ACCESS_TOKEN = "fp_access_token";
-export const FP_REFRESH_TOKEN = "fp_refresh_token";
-export const FP_ID_TOKEN = "fp_id_token";
+export const FP_ACCESS_TOKEN = 'fp_access_token';
+export const FP_REFRESH_TOKEN = 'fp_refresh_token';
+export const FP_ID_TOKEN = 'fp_id_token';
+export const FP_MERCHANT_ID_HEADER = 'X-FP-MERCHANT-ID';
