3fc5d032aea6b671f8e0b63a2b4ec1418e6a0451
norasotanext <minhbn@sotanext.com>
2022-05-27 14:32:20 +0700

    chore: update event name

--- events/merchant.constants.ts
+++ events/merchant.constants.ts
@@ -1,13 +1,15 @@
 export const MERCHANT_CONST_EVENTS = {
   MERCHANT_UPDATE_PAYMENT_SETTINGS: "merchant.update_payment_settings",
   MERCHANT_UPDATE_CRYPTO_SETTINGS: "merchant.update_crypto_settings",
-  MERCHANT_AUTHENTICATION_DEVICE_UPDATED:
-    "merchant.authentication_device_updated",
-  MERCHANT_PASSWORD_UPDATED: "merchant.password_updated",
-  MERCHANT_REMIND_BACKUP_CODES_EXCEED_LIMIT:
-    "merchant.remind_backup_codes_exceed_limit",
-  MERCHANT_SUPPORT_EMAIL_UPDATED: "merchant.support_email_updated",
-  MERCHANT_ACCEPT_CRYPTO_LIST: "merchant.accept_crypto_list",
-  MERCHANT_FLEXIBLE_PAYMENT_UPDATED: "merchant.flexible_payment_updated",
-  MERCHANT_AUTO_WITHDRAWAL_UPDATED: "merchant.auto_withdrawal_updated",
+  // Security
+  SECURITY_AUTHENTICATION_DEVICE_UPDATED:
+    "security.authentication_device_updated",
+  SECURITY_PASSWORD_UPDATED: "security.password_updated",
+  SECURITY_REMIND_BACKUP_CODES_EXCEED_LIMIT:
+    "security.remind_backup_codes_exceed_limit",
+  // Settings
+  SETTINGS_SUPPORT_EMAIL_UPDATED: "settings.support_email_updated",
+  SETTINGS_ACCEPT_CRYPTO_LIST: "settings.accept_crypto_list",
+  SETTINGS_FLEXIBLE_PAYMENT_UPDATED: "settings.flexible_payment_updated",
+  SETTINGS_AUTO_WITHDRAWAL_UPDATED: "settings.auto_withdrawal_updated",
 };
