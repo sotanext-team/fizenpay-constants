8fb2ff5ac4e8da0955ef5ce84ea484ec6556f54b
norasotanext <minhbn@sotanext.com>
2022-05-27 13:54:01 +0700

    feat: add event merchant

--- events/merchant.constants.ts
+++ events/merchant.constants.ts
@@ -1,4 +1,13 @@
 export const MERCHANT_CONST_EVENTS = {
-  MERCHANT_UPDATE_PAYMENT_SETTINGS: 'merchant.update_payment_settings',
-  MERCHANT_UPDATE_CRYPTO_SETTINGS: 'merchant.update_crypto_settings',
+  MERCHANT_UPDATE_PAYMENT_SETTINGS: "merchant.update_payment_settings",
+  MERCHANT_UPDATE_CRYPTO_SETTINGS: "merchant.update_crypto_settings",
+  MERCHANT_AUTHENTICATION_DEVICE_UPDATED:
+    "merchant.authentication_device_updated",
+  MERCHANT_PASSWORD_UPDATED: "merchant.password_updated",
+  MERCHANT_REMIND_BACKUP_CODES_EXCEED_LIMIT:
+    "merchant.remind_backup_codes_exceed_limit",
+  MERCHANT_SUPPORT_EMAIL_UPDATED: "merchant.support_email_updated",
+  MERCHANT_ACCEPT_CRYPTO_LIST: "merchant.accept_crypto_list",
+  MERCHANT_FLEXIBLE_PAYMENT_UPDATED: "merchant.flexible_payment_updated",
+  MERCHANT_AUTO_WITHDRAWAL_UPDATED: "merchant.auto_withdrawal_updated",
 };
---
+++ events/withdrawal.constants.ts
@@ -0,0 +1,3 @@
+export const WITHDRAWAL_CONST_EVENTS = {
+  WITHDRAWAL_STATUS_UPDATED: "withdrawal.status_updated",
+};
