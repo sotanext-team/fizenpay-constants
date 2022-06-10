e2f6dc941ec56f6c1bdd532d8658b9307081c4de
norasotanext <minhbn@sotanext.com>
2022-06-10 09:46:49 +0700

    chore: format code

--- settings/email-preferences.constants.ts
+++ settings/email-preferences.constants.ts
@@ -1,19 +1,19 @@
-export const OPERATION_CHARGE_CREATED = "operation-charge_created";
-export const OPERATION_CHARGE_EXPIRED = "operation-charge_expired";
+export const OPERATION_CHARGE_CREATED = 'operation-charge_created';
+export const OPERATION_CHARGE_EXPIRED = 'operation-charge_expired';
 export const OPERATION_PAYMENT_STATUS_UPDATED =
-  "operation-payment_status_updated";
+  'operation-payment_status_updated';
 export const OPERATION_WITHDRAWAL_STATUS_UPDATED =
-  "operation-withdrawal_status_updated";
-export const OPERATION_INVOICE_CREATED = "operation-invoice_created";
+  'operation-withdrawal_status_updated';
+export const OPERATION_INVOICE_CREATED = 'operation-invoice_created';
 export const SECURITY_AUTHENTICATION_DEVICE_UPDATED =
-  "security-authentication_device_updated";
-export const SECURITY_PASSWORD_UPDATED = "security-password_updated";
-export const SECURITY_CLOSE_ACCOUNT = "security-close_account";
+  'security-authentication_device_updated';
+export const SECURITY_PASSWORD_UPDATED = 'security-password_updated';
+export const SECURITY_CLOSE_ACCOUNT = 'security-close_account';
 export const SECURITY_REMIND_BACKUP_CODES_EXCEED_LIMIT =
-  "security-remind_backup_codes_exceed_limit";
-export const SETTINGS_SUPPORT_EMAIL_UPDATED = "settings-support_email_updated";
-export const SETTINGS_ACCEPT_CRYPTO_LIST = "settings-accept_crypto_list";
+  'security-remind_backup_codes_exceed_limit';
+export const SETTINGS_SUPPORT_EMAIL_UPDATED = 'settings-support_email_updated';
+export const SETTINGS_ACCEPT_CRYPTO_LIST = 'settings-accept_crypto_list';
 export const SETTINGS_FLEXIBLE_PAYMENT_UPDATED =
-  "settings-flexible_payment_updated";
+  'settings-flexible_payment_updated';
 export const SETTINGS_AUTO_WITHDRAWAL_UPDATED =
-  "settings-auto_withdrawal_updated";
+  'settings-auto_withdrawal_updated';
