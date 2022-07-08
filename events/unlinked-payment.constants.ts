9c0e9f129241ef5f5fdd150f0d8244686242bb4d
norasotanext <minhbn@sotanext.com>
2022-07-08 11:41:34 +0700

    feat: add event

--- events/unlinked-payment.constants.ts
+++ events/unlinked-payment.constants.ts
@@ -1,3 +1,5 @@
 export const UNLINKED_PAYMENT_CONST_EVENTS = {
   UNLINKED_PAYMENT_CREATED: 'unlinked-payment.created',
+  UNLINKED_PAYMENT_FORWARDED_TO_MERCHANT:
+    'unlinked-payment.forwarded-to-merchant',
 };
